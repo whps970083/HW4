3560749ef4356d5ab9e5bb9c6a4a518f4f63710b
whps970083 <whps970083@gmail.com>
2023-11-29 00:56:04 +0800

    Record count

--- kernel_driver/myioctl_driver.c
+++ kernel_driver/myioctl_driver.c
@@ -13,8 +13,7 @@ MODULE_AUTHOR("B11001035_劉家成");
 MODULE_DESCRIPTION("Simple IOCTL Example");
 
 static int myioctl_major;
-static int count = 0;
-
+static long count = 0;
 
 static int myioctl_open(struct inode *inode, struct file *filp);
 static int myioctl_release(struct inode *inode, struct file *filp);
@@ -26,6 +25,39 @@ static const struct file_operations myioctl_fops = {
     .unlocked_ioctl = myioctl_ioctl,
 };
 
+static const char *my_file_path = "/etc/my_variable_file";
+
+static void save_variable(void) {
+    struct file *file;
+    loff_t pos = 0;
+
+    file = filp_open(my_file_path, O_WRONLY | O_CREAT, 0644);
+    if (IS_ERR(file)) {
+        printk(KERN_ERR "Error opening file %s\n", my_file_path);
+        return;
+    }
+
+    kernel_write(file, &count, sizeof(count), &pos);
+
+    filp_close(file, NULL);
+}
+
+static void load_variable(void) {
+    struct file *file;
+    loff_t pos = 0;
+
+    file = filp_open(my_file_path, O_RDONLY, 0);
+    if (IS_ERR(file)) {
+        printk(KERN_ERR "Error opening file %s\n", my_file_path);
+        return;
+    }
+
+    kernel_read(file, &count, sizeof(count), &pos);
+
+    filp_close(file, NULL);
+}
+
+
 static int __init myioctl_init(void) {
     myioctl_major = register_chrdev(0, "myioctl", &myioctl_fops);
 
@@ -33,13 +65,16 @@ static int __init myioctl_init(void) {
         pr_err("Failed to register character device\n");
         return myioctl_major;
     }
-
+    load_variable();
     pr_info("my ioctl module loaded. Major number: %d\n", myioctl_major);
+
     return 0;
 }
 
 static void __exit myioctl_exit(void) {
+    save_variable();
     unregister_chrdev(myioctl_major, "myioctl");
+    
     pr_info("myioctl module unloaded\n");
 }
 
@@ -70,7 +105,7 @@ static long myioctl_ioctl(struct file *filp, unsigned int cmd, unsigned long arg
         
         case MYIOCTL_GET_COUNT:
             pr_info("IOCTL: Getting counter value\n");
-            err = copy_to_user((int *)arg, &count, sizeof(int));
+            err = copy_to_user((int *)arg, &count, sizeof(long));
             break;
         
         case MYIOCTL_INCREMENT:
